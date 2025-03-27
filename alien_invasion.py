2c6fddb0abef2330fc5062a5a3797fe024b04644
pan zi <2434649937@qq.com>
2025-03-27 13:43:06 +0800

    4 commmit

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion():
     """管理游戏资源和行为的类"""
@@ -14,21 +15,37 @@ class AlienInvasion():
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
 
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            #监听键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_event()
+            self.ship.update()
+            self._update_screen()
 
-            #每次循环都重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-
-            #让最近绘制的屏幕可见
-            pygame.display.flip()
             self.clock.tick(60)
+
+    def _check_event(self):
+        # 监听键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+
+    def _update_screen(self):
+        # 每次循环都重绘屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #创建游戏实例并运行游戏
