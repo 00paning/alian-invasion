ffdb85901397ae0823a110b7081c01efcf95bbf6
pan zi <2434649937@qq.com>
2025-03-27 18:13:56 +0800

    4 commmit

--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self,ai_game):
         """初始化飞船并设置其初始位置"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #加载飞船图像并获取外部形状
@@ -14,13 +15,22 @@ class Ship:
         #每艘飞船都放在屏幕底部的中央
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #在飞船的属性中储存一个浮点数
+        self.x = float(self.rect.x)
+
         #移动标志
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         """根据移动标志调整飞船位置"""
-        if self.moving_right:
-            self.rect.x += 1
+        if self.moving_right and self.moving_right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.moving_left > 0:
+            self.x -= self.settings.ship_speed
+
+        #根据self.x更新rect对象
+        self.rect.x = self.x
 
     def blitme(self):
         """在指定位置绘制飞船"""
