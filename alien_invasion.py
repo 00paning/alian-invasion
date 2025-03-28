c53d84e218ecb7977b8089bd9f0694f3ac6f1e69
pan zi <2434649937@qq.com>
2025-03-28 21:00:22 +0800

    finial

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,10 @@ from settings import Settings
 from ship import Ship
 from bullet import Bullet
 from alien import  Alien
+from time import sleep
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
 
 class AlienInvasion():
     """管理游戏资源和行为的类"""
@@ -19,20 +23,32 @@ class AlienInvasion():
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
+        #创建一个用于存储游戏统计信息的实例
+        self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
+
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
 
+        #游戏启动后处于非存活状态
+        self.game_active = False
+
+        #创造play按钮
+        self.play_button = Button(self,"play")
+
 
     def run_game(self):
         """开始游戏的主循环"""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
 
+            if self.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
             self._update_screen()
 
             self.clock.tick(60)
@@ -46,6 +62,33 @@ class AlienInvasion():
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self,mouse_pos):
+        """在玩家点击play时开始新的游戏"""
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
+            #还原游戏设置
+            self.settings.initialize_dynamic_settings()
+            #重置游戏的统计信息
+            self.stats.reset_stats()
+            self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
+            self.game_active = True
+
+            #清空外星人列表和子弹列表
+            self.bullets.empty()
+            self.aliens.empty()
+
+            #创造一个新的外星舰队，并将飞船放在屏幕下方的中央
+            self._create_fleet()
+            self.ship.center_ship()
+
+            #隐藏光标
+            pygame.mouse.set_visible(False)
 
 
     def _check_keydown_events(self,event):
@@ -79,6 +122,13 @@ class AlienInvasion():
         self.ship.blitme()
         self.aliens.draw(self.screen)
 
+        #显示得分
+        self.sb.show_score()
+
+        #如果游戏处于非存活状态，就绘制play按钮
+        if not self.game_active:
+            self.play_button.draw_button()
+
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
@@ -90,11 +140,102 @@ class AlienInvasion():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        self._check_bullet_alien_collisions()
+
     def _create_fleet(self):
         """创造外星舰队"""
         #创造一个外星人
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width,alien_height = alien.rect.size
+
+        current_x,current_y = alien_width,alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._creat_alien(current_x,current_y)
+                current_x += 2 * alien_width
+
+            #添加一个外星人后，重置x值并递增y值
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _creat_alien(self,x_position,y_position):
+        """创造一个外星人，并将其放入外星舰队"""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+        """有外星人到达边界时采取响应的措施"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """将整个外星舰队向下移动，并改变方向"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+    def _update_aliens(self):
+        """更新外星人的位置"""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+        #检测外星人和飞船之间的碰撞
+        if pygame.sprite.spritecollideany(self.ship,self.aliens):
+            self._ship_hit()
+
+        #检查是否有外星人到达了屏幕的下边缘
+        self._check_aliens_bottom()
+
+    def _check_bullet_alien_collisions(self):
+        # 检测是否有子弹击中了外星人
+        # 如果是，就删除相应的子弹和外星人
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
+
+        if not self.aliens:
+            # 删除现有的子弹创造一个新的外星舰队
+            self.bullets.empty()
+            self._create_fleet()
+            self.settings.increase_speed()
+
+            #提高等级
+            self.stats.level += 1
+            self.sb.prep_level()
+
+    def _ship_hit(self):
+        """响应飞船和外星人的碰撞"""
+        if self.stats.ships_left > 0:
+            #将ships_left减1并更新计分板
+            self.stats.ships_left -= 1
+            self.sb.prep_ships()
+            self.bullets.empty()
+            self.aliens.empty()
+
+            self._create_fleet()
+            self.ship.center_ship()
+
+            sleep(0.5)
+        else:
+            self.game_active = False
+            pygame.mouse.set_visible(True)
+
+    def _check_aliens_bottom(self):
+        """检查是否有外星人到达了屏幕的下边缘"""
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= self.settings.screen_height:
+                #像飞船一样被处理
+                self._ship_hit()
+                break
 
 if __name__ == '__main__':
     #创建游戏实例并运行游戏
